37b9ee02cc7b6d83f83f42cb047598c3c136058e
pjfsu <pedrosanchezuscamaita@gmail.com>
2024-12-28 03:21:25 +0100

    DONE: pdftk instead of pdfbox, xsltproc instead of xalan and xmllint instad of xerces. TODO: diagram and test

--- split_book.sh
+++ split_book.sh
@@ -8,28 +8,38 @@
 # repository:github.com/pjfsu/split_book # 
 ##########################################
 
-check_args_length_is_two()
+# GLOBALS
+declare -ri ARGS_LENGTH=2
+declare -r THIS_SCRIPT_DIR="$( dirname "$( realpath "${0}" )" )"
+declare -r TMP_CONFIG_XML="/tmp/split_book.config.xml"
+declare -r TMP_VALID_CHAPTERS_CSV="/tmp/split_book.valid_chapters.csv"
+declare -r TMP_INVALID_CHAPTERS_CSV="/tmp/split_book.invalid_chapters.csv"
+
+# SETS
+set -e # if something fails, then exits
+
+check_args_length()
 {
 	# description
-	#	it checks if this script arguments length is two
+	#	it checks this script args length
 	# arguments
-	#	${1}:int, this script arguments length
+	#	${1}:int, this script args length
 	# globals
-	#	none
+	#	ARGS_LENGTH
 	# preconditions
 	#	i. ${1} >= 0
 	# returns
-	#	0, if ${1} == 2
+	#	0, if ${1} == ARGS_LENGTH
 	#	1, other case
 
-	[ ${1} -eq 2 ] \
+	[ ${1} -eq ${ARGS_LENGTH} ] \
 		&& return 0
 
-	printf '[Error] expected two arguments, usage: "./split_book.sh PDF XML"\n' >&2
+	printf '[Error] expected %i arguments, usage: "./split_book.sh PDF XML"\n' ${ARGS_LENGTH} >&2
 	return 1
 }
 
-check_book_pdf_exists()
+check_pdf_exists()
 {
 	# description
 	#	it checks if pdf exists
@@ -67,14 +77,14 @@ check_chapters_xml_exists()
 	[ -f "${1}" ] \
 		&& return 0
 
-	printf '[Error] not found chapters xml "%s"\n' "${1}" >&2
+	printf '[Error] not found xml "%s"\n' "${1}" >&2
 	return 1
 }
 
-check_book_pdf_is_a_pdf_file()
+check_pdf_is_valid()
 {
 	# description
-	#	it checks if pdf is a valid pdf
+	#	it checks if pdf is valid
 	# arguments
 	#	${1}:str, pdf absolute or relative path
 	# globals
@@ -86,7 +96,7 @@ check_book_pdf_is_a_pdf_file()
 	#	0, if ${1} is a valid pdf
 	#	1, other case
 
-	printf '[INFO] validating book "%s" ...\n' "${1}"
+	printf '[INFO] validating pdf "%s" ...\n' "${1}"
 	pdfinfo "${1}" \
 		&& return 0
 
@@ -102,7 +112,7 @@ generate_config_xml()
 	#	${1}:str, pdf absolute path
 	#	${2}:str, chapters xml absolute path
 	# globals
-	#	CONFIG_XML
+	#	TMP_CONFIG_XML
 	# preconditions
 	#	i. ${1} is a valid pdf
 	#	ii. ${2} exists
@@ -110,29 +120,19 @@ generate_config_xml()
 	# returns
 	#	0
 
-	local -ri book_pdf_total_pages=$( pdfinfo "${1}" | grep Pages | cut -d: -f2 )
-	local -r outdir="${1%.pdf}"
-	local -r chapters_xml_content="$( cat "${2}" )"
-	local -r pdfbox_dir="${THIS_SCRIPT_DIR}/pdfbox"
-	local -r pdfbox_app_jar="${pdfbox_dir}/pdfbox-app-3.0.3.jar"
-	local -r config_xml_template="<!-- BEGIN OF CONFIG XML -->
-<split_book 
-pdfbox_app_jar=\"${pdfbox_app_jar}\"
-outdir=\"%s\">
-<book
-name=\"%s\"
-pages=\"%i\">
-<!-- BEGIN OF CHAPTERS XML -->
+	local -ri pdf_total_pages=$( pdfinfo "${1}" | grep Pages | cut -d: -f2 )
+	local -r config_xml_template='<split_book>
+<book pages="%i">
+<chapters>
 %s
-<!-- END OF CHAPTERS XML -->
+</chapters>
 </book>
-</split_book>
-<!-- END OF CONFIG XML -->"
+</split_book>'
 
-	printf '[INFO] generating xml "%s" ...\n' "${CONFIG_XML}"
+	printf '[INFO] generating config xml "%s" ...\n' "${TMP_CONFIG_XML}"
 	printf "${config_xml_template}\n" \
-		"${outdir}" "${1}" ${book_pdf_total_pages} "$( cat "${2}" )" \
-		| tee "${CONFIG_XML}"
+		${pdf_total_pages} "$( cat "${2}" )" \
+		| tee "${TMP_CONFIG_XML}"
 
 	return 0
 }
@@ -140,83 +140,107 @@ pages=\"%i\">
 validate_config_xml()
 {
 	# description
-	#	it validates config xml against config xsd with apache xerces
+	#	it validates config xml against xsd
 	# arguments
 	#	none
 	# globals
-	#	CONFIG_DIR
-	#	CONFIG_XML
+	#	THIS_SCRIPT_DIR
+	#	TMP_CONFIG_XML
 	# preconditions
-	#	i. generated config xml exists
-	#	ii. /bin/java exists
+	#	i. TMP_CONFIG_XML was generated
+	#	ii. /bin/xmllint exists
 	# returns
 	#	0, if config xml is valid
 	#	1, other case
 
-	local -r config_xsd_="${CONFIG_DIR}/config.xsd"
-	local -r xerces_dir="${THIS_SCRIPT_DIR}/xerces/xerces-2_12_2"
-	local -r xerces_samples_jar="${xerces_dir}/xercesSamples.jar"
-	local -r xerces_impl_jar="${xerces_dir}/xercesImpl.jar"
-	local -r error_log="/tmp/split_book.error.log"
+	local -r xsd="${THIS_SCRIPT_DIR}/xsd/config.xsd"
 
-	printf '[INFO] validating xml "%s" ...\n' "${CONFIG_XML}"
-	# java always returns zero, so java's errors are redirected to a temporary file and to stdout
-	# ty! https://stackoverflow.com/questions/692000/how-do-i-write-standard-error-to-a-file-while-using-tee-with-a-pipe
-	java -cp "${xerces_samples_jar}:${xerces_impl_jar}" jaxp.SourceValidator \
-		-a "${config_xsd_}" \
-		-i "${CONFIG_XML}" \
-		2> >(tee "${error_log}" >&2)
-	[ -s "${error_log}" ] \
-		&& return 1
+	printf '[INFO] validating config xml "%s" against xsd "%s" ...\n' \
+		"${TMP_CONFIG_XML}" "${xsd}"
+	xmllint --noout --schema "${xsd}" "${TMP_CONFIG_XML}" \
+		|| return 1
 
 	return 0
 }
 
-transform_config_xml_to_pdfbox_script()
+transform_config_xml()
 {
 	# description
-	#	it transforms config xml to pdfbox script using config xsl with apache xalan
+	#	it transforms config xml using xslt
 	# arguments
 	#	none
 	# globals
-	#	CONFIG_XML
-	#	PDFBOX_SCRIPT
+	#	THIS_SCRIPT_DIR
+	#	TMP_CONFIG_XML
+	#	TMP_VALID_CHAPTERS_CSV
+	#	TMP_VALID_CHAPTERS_CSV
 	# preconditions
-	#	i. generated config xml exists
-	#	ii. /bin/java exists
+	#	i. TMP_CONFIG_XML was generated
+	#	ii. /bin/xsltproc exists
 	# returns
 	#	0
 
-	local -r config_xsl_="${CONFIG_DIR}/config.xsl"
-	local -r xalan_dir="${THIS_SCRIPT_DIR}/xalan/xalan-j_2_7_3"
-	local -r xalan_jar="${xalan_dir}/xalan.jar"
+	local -r xsl_dir="${THIS_SCRIPT_DIR}/xsl"
+	local -r valid_chapters_xsl="${xsl_dir}/valid_chapters.xsl"
+	local -r invalid_chapters_xsl="${xsl_dir}/invalid_chapters.xsl"
 
-	printf '[INFO] transforming xml "%s" into script "%s" ...\n' \
-		"${CONFIG_XML}" "${PDFBOX_SCRIPT}"
-	java -cp "${xalan_jar}" org.apache.xalan.xslt.Process \
-		-XSL "${config_xsl_}" \
-		-IN "${CONFIG_XML}" \
-		-OUT "${PDFBOX_SCRIPT}"
-	cat "${PDFBOX_SCRIPT}"
+	printf '[INFO] transforming config xml "%s" into csv "%s" ...\n' \
+		"${TMP_CONFIG_XML}" "${TMP_VALID_CHAPTERS_CSV}"
+	xsltproc "${valid_chapters_xsl}" "${TMP_CONFIG_XML}" \
+		| tee "${TMP_VALID_CHAPTERS_CSV}"
+	printf '[INFO] transforming config xml "%s" into csv "%s" ...\n' \
+		"${TMP_CONFIG_XML}" "${TMP_INVALID_CHAPTERS_CSV}"
+	xsltproc "${invalid_chapters_xsl}" "${TMP_CONFIG_XML}" \
+		| tee "${TMP_INVALID_CHAPTERS_CSV}"
 
 	return 0
 }
 
-run_pdfbox_script()
+split()
 {
 	# description
-	#	it runs pdfbox script
+	#	it splits the pdf
 	# arguments
-	#	none
+	#	${1}:str, pdf absolute path
 	# globals
-	#	PDFBOX_SCRIPT
+	#	TMP_VALID_CHAPTERS_CSV
+	#	TMP_VALID_CHAPTERS_CSV
 	# preconditions
-	#	i. generated pdfbox script exists
+	#	i. TMP_VALID_CHAPTERS_CSV was generated
+	#	ii. TMP_VALID_CHAPTERS_CSV was generated
+	#	iii. /bin/pdftk exists
 	# returns
 	#	0
 
-	printf '[INFO] running script "%s" ...\n' "${PDFBOX_SCRIPT}"
-	bash "${PDFBOX_SCRIPT}"
+	local -r outdir="${1%.pdf}" # it removes the ".pdf" extension
+	local -i from=0
+	local -i to=0
+	local chapter=""
+	local reason=""
+
+	! [ -d "${outdir}" ] \
+		&& printf 'creating directory "%s" ...\n' "${outdir}" \
+		&& mkdir "${outdir}"
+
+	while read valid_chapter
+	do
+		from=$( cut -d, -f1 <<< "${valid_chapter}" )
+		to=$( cut -d, -f2 <<< "${valid_chapter}" )
+		chapter="$( cut -d, -f3- <<< "${valid_chapter}" )"
+		printf '[INFO] splitting chapter "%s" ... ' "${chapter}"
+		pdftk "${1}" \
+			cat ${from}-${to} \
+			output "${outdir}/${chapter}.pdf"
+		printf "done!\n"
+	done < "${TMP_VALID_CHAPTERS_CSV}"
+
+	while read invalid_chapter
+	do
+		reason="$( cut -d, -f1 <<< "${invalid_chapter}" )"
+		chapter="$( cut -d, -f2- <<< "${invalid_chapter}" )"
+		printf '[ERROR] invalid chapter "%s" because "%s"\n' \
+			"${chapter}" "${reason}"
+	done < "${TMP_INVALID_CHAPTERS_CSV}"
 
 	return 0
 }
@@ -253,33 +277,28 @@ main()
 	#	${1}:str, pdf relative or absolute path
 	#	${2}:str, chapters xml relative or absolute path
 	# globals
-	#	none
+	#	ARGS_LENGTH
+	#	TMP_CONFIG_XML	
+	#	TMP_VALID_CHAPTERS_CSV	
+	#	TMP_INVALID_CHAPTERS_CSV	
 	# preconditions
 	#	user runs this script
 	# returns
 	#	0, if ok
 	#	11, if arguments length is not two
-	#	13, if pdf does not exist
-	#	17, if chapters xml does not exist
-	#	19, if pdf is not a pdf file
+	#	13, if pdf doesn't exist
+	#	17, if chapters xml doesn't exist
+	#	19, if pdf isn't valid
 	#	23, if config xml is not well formed (see config.xsd)
 
-	check_args_length_is_two "${#}" || return 11
-	check_book_pdf_exists "${1}" || return 13
+	check_args_length "${#}" || return 11
+	check_pdf_exists "${1}" || return 13
 	check_chapters_xml_exists "${2}" || return 17
-	check_book_pdf_is_a_pdf_file "${1}" || return 19
-
-	# at this point, by default, absolute path is used
-	local -r THIS_SCRIPT_DIR="$( dirname "$( realpath "${0}" )" )"
-	local -r CONFIG_DIR="${THIS_SCRIPT_DIR}/config"
-	local -r CONFIG_XML="/tmp/split_book.config.xml"
-	local -r PDFBOX_SCRIPT="/tmp/split_book.pdfbox.sh"
-
+	check_pdf_is_valid "${1}" || return 19
 	generate_config_xml "$( realpath "${1}" )" "$( realpath "${2}" )"
 	validate_config_xml || return 23
-	transform_config_xml_to_pdfbox_script
-	# un/comment next line to run/debug pdfbox script
-	run_pdfbox_script
+	transform_config_xml
+	split "$( realpath "${1}" )"
 	ty
 
 	return 0
