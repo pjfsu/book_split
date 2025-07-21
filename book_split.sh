d9ea777ae0368980bfd74abe83fcfb3cb6cfdea0
pjfsu <pedrosanchezuscamaita@gmail.com>
2025-07-21 19:58:04 +0200

    Containerfile: appuser, env; run: userns keep-id and -u

--- book_split.sh
+++ book_split.sh
@@ -1,23 +1,46 @@
 #!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail
+set -o errexit -o nounset -o pipefail -o errtrace #-o xtrace
 IFS=$'\n\t'
 
-readonly BOOK="$(realpath ${1})"
-readonly CHAPTERS="$(realpath ${2})"
-readonly OUT_DIR="${BOOK%.*}"
-readonly CONTAINERFILE="$(find $HOME -type f -wholename "*/book_split/Containerfile" 2> /dev/null)"
-readonly BOOK_SPLIT_IMG="localhost/book_split:1.3.3"
+BOOK="$(realpath "${1}")"
+CHAPTERS="$(realpath "${2}")"
+OUT_DIR="${BOOK%.*}"
+
+PDFTK_CONTAINERFILE="$( find "${HOME}" \
+	\( -name '.*' -prune \) \
+	-o \
+	\( -type f -wholename "*/book_split/pdftk/Containerfile" -print \) 
+)"
+PDFTK_IMAGE="localhost/pdftk:1.0.0"
+
+APP_CONTAINERFILE="$( find "${HOME}" \
+	\( -name '.*' -prune \) \
+	-o \
+	\( -type f -wholename "*/book_split/app/Containerfile" -print \) 
+)"
+APP_SCRIPT="$( find "${HOME}" \
+	\( -name '.*' -prune \) \
+	-o \
+	\( -type f -wholename "*/book_split/app/app.sh" -print \) 
+)"
+APP_IMAGE="localhost/book_split:1.3.3"
 
 [ ! -d "${OUT_DIR}" ] && mkdir -p "${OUT_DIR}"
 
-! podman image exists "${BOOK_SPLIT_IMG}" \
-	&& podman build -t "${BOOK_SPLIT_IMG}" -f "${CONTAINERFILE}"
+! podman image exists "${PDFTK_IMAGE}" \
+	&& podman build -t "${PDFTK_IMAGE}" -f "${PDFTK_CONTAINERFILE}"
+
+! podman image exists "${APP_IMAGE}" \
+	&& podman build -t "${APP_IMAGE}" -f "${APP_CONTAINERFILE}"
 
 podman run --rm \
+	--userns=keep-id \
+	--user $(id -u):$(id -g) \
+	-v "${APP_SCRIPT}:/app/app.sh:ro,Z" \
 	-v "${BOOK}:/app/in/book.pdf:ro,Z" \
 	-v "${CHAPTERS}:/app/in/chapters.csv:ro,Z" \
 	-v "${OUT_DIR}:/app/out:Z" \
-	"${BOOK_SPLIT_IMG}"
+	"${APP_IMAGE}"
 
 # thanks for using this program!
 # grazas por usar este programa!
